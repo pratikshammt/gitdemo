d4d6979cada46529c04a162f181a91059f442fc7
Ashwini <ashwini.joshi@mediamagictechnologies.com>
2019-12-31 16:14:20 +0530

    3rd commit om feacture_a

--- prog.cpp
+++ prog.cpp
@@ -23,3 +23,43 @@ class FourWheeler : public Vehicle {
         cout << "I am four wheeler" << endl; 
     } 
 }; 
+Vehicle* Vehicle::Create(VehicleType type) { 
+    if (type == VT_TwoWheeler) 
+        return new TwoWheeler(); 
+    else if (type == VT_ThreeWheeler) 
+        return new ThreeWheeler(); 
+    else if (type == VT_FourWheeler) 
+        return new FourWheeler(); 
+    else return NULL; 
+} 
+  
+// Client class 
+class Client { 
+public: 
+  
+    // Client doesn't explicitly create objects 
+    // but passes type to factory method "Create()" 
+    Client() 
+    { 
+        VehicleType type = VT_ThreeWheeler; 
+        pVehicle = Vehicle::Create(type); 
+    } 
+    ~Client() { 
+        if (pVehicle) { 
+            delete[] pVehicle; 
+            pVehicle = NULL; 
+        } 
+    } 
+    Vehicle* getVehicle()  { 
+        return pVehicle; 
+    } 
+  
+private: 
+    Vehicle *pVehicle; 
+}; 
+int main() { 
+    Client *pClient = new Client(); 
+    Vehicle * pVehicle = pClient->getVehicle(); 
+    pVehicle->printVehicle(); 
+    return 0; 
+}
